e366b6d599957b078ebccdfc4be10ddb92ea0947
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-12-31 01:30:02 +0530

    docs: add docs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Queue - NodeJS
+
+## Sequential Processing in non-blocking code
+
+Run `node ./test.js`
+Build `tsc -p .`
+
+## TODO
+
+-   Remove event dependency.
+-   Make trigger mechanism.
--- src/index.ts
+++ src/index.ts
@@ -34,33 +34,9 @@ export class queue {
 				this.list_of_functions = this.list_of_functions.slice(1)
 
 				console.log(this.list_of_values[current_head.index], 'top')
-				// console.log(this.list_of_values[current_head.index], current_head.index, this.list_of_functions.length)
 				;(process as any).emit(`resolved ${current_head.index}` as any, this.list_of_values[current_head.index])
 			}
 			this.processing = false
 		}
 	}
 }
-
-/**Driver Code */
-// console.log('start')
-// const q = new queue()
-// q.poll()
-// for (let i = 0; i < 10; i++) {
-// 	const index = q.enqueue(() => {
-// 		const p = new Promise((resolve) => {
-// 			setTimeout(() => resolve('ok' + i), 1000)
-// 		})
-// 		return p
-// 	})
-// 	process.on(`resolved ${index}` as any, (data) => {
-// 		console.log(data)
-// 	})
-// }
-
-// q.enqueue(() => {
-// 	const p = new Promise((resolve) => {
-// 		setTimeout(() => resolve('ok'), 1000)
-// 	})
-// 	return p
-// })
